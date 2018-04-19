4681f2f6102f408f8566f8d2eb33486d406dd5ab
wjh <wjhlily@126.com>
2018-04-19 14:22:44 +0800

    Initial Commit

---
+++ WYhouseInfoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYhouseInfoDemo
+//
+//  Created by WY on 2018/4/19.
+//  Copyright © 2018年 WY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WYhouseInfoDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WYhouseInfoDemo
+//
+//  Created by WY on 2018/4/19.
+//  Copyright © 2018年 WY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
